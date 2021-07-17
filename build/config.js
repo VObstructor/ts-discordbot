58ff51f3629b2e72f1723c31cebab4f30a8f6d44
VObstructor <50927656+VObstructor@users.noreply.github.com>
2021-07-17 17:47:38 +0200

    rebuild v27
    moved alot of files so the building process will be more stable

--- build/config.js
+++
@@ -1,7 +0,0 @@
-{
-    "client_id" = process.env.client-id,
-    "client_secret" =  process.env.client-secret,
-    "scope" = process.env.range,
-    "discord" = process.env.token,
-    "groupId" = process.env.group-id
-}
