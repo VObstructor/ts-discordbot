44efb9cf00bd6e5c71f9401e64c48bfdc1bde0b8
VObstructor <50927656+VObstructor@users.noreply.github.com>
2021-07-24 00:56:27 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ const whitelist = require('../whitelisteditems');
 
 import * as att from 'js-tale/dist';
 import discord from 'discord.js';
-import { config } from './config';
+import { config } from '../src/config';
 
 
 var discordBot:discord.Client;
