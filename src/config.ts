2012f796b3ba9451c5eed6c16b5f178be9667112
VObstructor <50927656+VObstructor@users.noreply.github.com>
2021-07-29 16:52:24 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -10,7 +10,7 @@ type botconfig = {
 export const config: botconfig = {
     client_id: process.env.clientId || '',
     client_secret:  process.env.clientSecret || '',
-    scope: 'ws.group ws.group_members ws.group_servers ws.group_bans ws.group_invites group.info group.join group.leave group.view group.members group.invite server.view server.console',
+    scope: "ws.group ws.group_members ws.group_servers ws.group_bans ws.group_invites group.info group.join group.leave group.view group.members group.invite server.view server.console",
     token: process.env.token || '' ,
     groupId: 1286637407
 };
